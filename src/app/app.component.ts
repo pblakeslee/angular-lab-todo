6268817c9f96a0f2ee18c0d1fec15a626b783d67
pbla2394@gmail.com <pbla2394@gmail.com>
2021-03-31 22:42:41 -0400

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { Todo } from 'src/app/todo';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  listItems: Todo[] = [
+    {
+      task: 'fold clothes',
+      completed: false,
+    },
+    {
+      task: 'put clothes in dresser',
+      completed: false,
+    },
+    {
+      task: 'relax',
+      completed: false,
+    },
+    {
+      task: 'try to pet cat',
+      completed: true,
+    },
+    {
+      task: 'pet dog',
+      completed: false,
+    },
+    {
+      task: 'be awesome',
+      completed: false,
+    },
+  ];
+
+  title = 'to-do-list';
+}
---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Todo } from '../todo';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css'],
+})
+export class TodoComponent implements OnInit {
+  @Input() listItem: Todo | null = null;
+  constructor() {}
+
+  ngOnInit(): void {}
+}
